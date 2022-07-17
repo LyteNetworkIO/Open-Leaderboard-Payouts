5cd60634a297cb4dfa607dc1992728ea54cb5f6d
Lyte <andygolay@gmail.com>
2022-07-17 11:18:22 +0000

    added some notes

--- package/index.js
+++ package/index.js
@@ -1,4 +1,15 @@
-function olp(test) {
+function olp(amount, leaderboard, curve) {
+
+    // Get the number of winners from the leaderboard array
+
+    // Create an array of winners' paymails, in order of leaderboard position from least to greatest
+    // Note: this could be confusing, because people think of "1" as the greatest ranking.
+    // Here, "position" refers to place in the array, starting with i=0.
+
+    //iterate over the array of paymails, and pay out the respective amount.
+
+
+    
     console.log(test);
 }
 
